51ab9a7389da0a5dd47767e87d2d8def260df62a
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2024-01-19 02:14:53 +0900

    bottomNavigationBar

--- app/src/main/java/com/devlog/article/MainActivity.kt
+++ app/src/main/java/com/devlog/article/MainActivity.kt
@@ -2,10 +2,42 @@ package com.devlog.article
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.devlog.article.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().add(R.id.containers, HomeFragment()).commit()
+
+        binding.bottomNavigationview.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bookmark -> {
+                    replaceFragment(BookmarkFragment())
+                    true
+                }
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
+
+    // 화면 전환 구현 메소드
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.containers, fragment).commit()
+    }
+
 }
