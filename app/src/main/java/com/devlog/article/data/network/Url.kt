ef93c8ae90ca97a2879bca8c9d579283a5b58308
polaris <61411085+polaris428@users.noreply.github.com>
2024-05-10 11:21:49 +0900

    서버 AWS이전

--- app/src/main/java/com/devlog/article/data/network/Url.kt
+++ app/src/main/java/com/devlog/article/data/network/Url.kt
@@ -1,6 +1,6 @@
 package com.devlog.article.data.network
 
 object Url {
-    const val PRODUCT_BASE_URL="https://article-back-a3007b45852a.herokuapp.com/api/"
+    const val PRODUCT_BASE_URL="https://api.logcat.kr/api/"
     const val NAVER_BASE_URL="https://naveropenapi.apigw.ntruss.com/"
 }
